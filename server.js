238e1fddfb0d84b817dd78bb3c3a3a4cd3955c94
Abdel7004 <abdelrahmanh15@gmail.com>
2023-04-12 12:56:04 -0400

    Revised MVC setup, and server.js setup

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require('express');
+const morgan = require('morgan'); 
+const methodOverride = require('method-override');
+const app = express();
+require('dotenv').config();
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")) //logging
+app.use(methodOverride("_method")) // override for put and delete requests from forms
+app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
+app.use(express.static("public")) // serve files from public statically
+
+app.get('/', (req, res) => {
+    res.send('default route')
+})
+
+const carsController = require('./controllers/cars');
+app.use('/cars', carssController);
+
+// Listener
+app.listen(3000, () =>
+	console.log(`express is listening on port: 3000`)
+);
