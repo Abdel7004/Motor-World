7e1a9b0ae40b5449bb6f98a5983ea871148c535b
Abdel7004 <abdelrahmanh15@gmail.com>
2023-04-14 09:04:31 -0400

    revise & corrected ejs

--- db/carSeedData.js
+++ db/carSeedData.js
@@ -1,7 +1,9 @@
 module.exports = [
-	{ model: '2023 Audi RS7', price: '$123,900', horsepower: '591 hp', image: 'https://media.ed.edmunds-media.com/audi/rs-7/2021/oem/2021_audi_rs-7_sedan_base_fq_oem_2_1280.jpg', salvaged: false },
+	{ model: '2023 Audi RS7', price: '$123,900', horsepower: '591 hp', image: 'https://i.imgur.com/0jZmNC0.jpg', salvaged: false },
 
-    { model: '2023 Porsche 911 GT3 RS', price: '$223,800', horsepower: '518 hp', image: 'https://s3.us-east-2.amazonaws.com/dealer-inspire-vps-vehicle-images/067c-110006864/thumbnails/large/WP0AC2A92PS270494/62c4609fd95ca466b848bfca66481adf.jpg', salvaged: false },
+    { model: '2023 Porsche 911 GT3 RS', price: '$223,800', horsepower: '518 hp', image: 'https://i.imgur.com/kwbQXSX.jpg', salvaged: false },
 
-    { model: '2014 Mercedes-Benz SLS AMG', price: '$208,000', horsepower: '622 hp', image: 'https://images.hgmsites.net/hug/mercedes-benz-sls-amg_100409076_h.jpg', salvaged: true },
+    { model: '2014 Mercedes-Benz SLS AMG', price: '$208,000', horsepower: '622 hp', image: 'https://i.imgur.com/4RSuniV.jpg', salvaged: true },
+
+	{ model: '2014 BMW i8', price: '$2,000', horsepower: '228 hp', image: 'https://i.imgur.com/zQcEGOu.jpg', salvaged: false },
 ];
