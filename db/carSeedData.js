64fc1db1eea9c473a5d08f899bbe80bf2129d13e
Abdel7004 <abdelrahmanh15@gmail.com>
2023-04-18 09:37:06 -0400

    class added to delete  button and changes made to css

--- db/carSeedData.js
+++ db/carSeedData.js
@@ -1,7 +1,7 @@
 module.exports = [
-	{ model: '2023 Audi RS7', price: '$123,900', horsepower: '591 hp', image: 'https://i.imgur.com/0jZmNC0.jpg', salvaged: false },
+	{ model: '2023 Audi RS7', price: '$123,900', horsepower: '591 hp', image: 'https://i.imgur.com/0jZmNC0.jpg', salvaged: true },
 
-    { model: '2023 Porsche 911 GT3 RS', price: '$223,800', horsepower: '518 hp', image: 'https://i.imgur.com/kwbQXSX.jpg', salvaged: false },
+    { model: '2023 Porsche 911 GT3 RS', price: '$223,800', horsepower: '518 hp', image: 'https://i.imgur.com/kwbQXSX.jpg', salvaged: true },
 
     { model: '2014 Mercedes-Benz SLS AMG', price: '$208,000', horsepower: '622 hp', image: 'https://i.imgur.com/4RSuniV.jpg', salvaged: true },
 
