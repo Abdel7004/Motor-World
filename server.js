b867c44d961d488f5f2616623b56db7ea9ed894c
Abdel7004 <abdelrahmanh15@gmail.com>
2023-04-20 20:37:28 -0400

    Final revisions

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors())
-app.get('/cars', (req, res) => {
+app.get('/', (req, res) => {
     res.send('default route')
 })
 
